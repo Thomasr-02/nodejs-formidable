9150d9e890378a08e337d7438122afa2b0194bdf
Thomas Ribeiro <thomas10191@hotmail.com>
2021-06-14 11:19:18 -0300

    Learning more about fs, how create and manage files and dir

--- src/index.js
+++ src/index.js
@@ -12,6 +12,9 @@ const fs = require("fs").promises;
 
 const PORT = 3000;
 const SECRET = "Teste";
+const CURRENT_DIR = path.resolve(__dirname, "..");
+const FILE_GREETINGS = path.resolve(__dirname, "..", "greetings.txt");
+const PARAMETERS_QUANTITY = 1;
 
 const app = express();
 
@@ -49,20 +52,48 @@ const crypto = () => {
   console.log(CryptoJS.SHA256("Message").words.toString("hex"));
 };
 
-const enc = (plainText) => {
-  const b64 = CryptoJS.AES.encrypt(plainText, SECRET).toString();
+const enc = () => {
+  const b64 = CryptoJS.AES.encrypt(SECRET, SECRET).toString();
   const e64 = CryptoJS.enc.Base64.parse(b64);
   const eHex = e64.toString(CryptoJS.enc.Hex);
   return eHex;
 };
 
-const readFile = async (filePath)=>{
+const readFile = async () => {
   try {
-    const data = await fs.readFile(filePath);
+    const data = await fs.readFile(FILE_GREETINGS);
     console.log(data.toString());
   } catch (error) {
     console.error(`Got an error trying to read the file: ${error.message}`);
   }
+};
+
+const readDir = async () => {
+  try {
+    const filesOnDirectory = await fs.readdir(CURRENT_DIR);
+    console.log(filesOnDirectory);
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+const copieFiles = async () => {
+  const random_name = "./tmp/"+ Math.random().toString(36).substring(7) + ".txt";
+  fs.copyFile(FILE_GREETINGS, random_name, PARAMETERS_QUANTITY, (error) => {
+    if (error) {
+      console.log(error);
+    }
+  });
+};
+
+const createDir = async (newDir) =>{
+  const existDir = 1;
+  const findDir = (await fs.readdir(CURRENT_DIR)).filter(dir => dir===newDir);
+  if(findDir.length === existDir){
+    console.log("already exist directory:", newDir);
+    return;
+  }
+  await fs.mkdir(newDir);
 }
 
 app.listen(PORT, (req, res) => {
@@ -70,8 +101,11 @@ app.listen(PORT, (req, res) => {
   console.log("\n----------------------------------------");
   console.log("Learning more about CRYPTO-JS\n");
   crypto();
-  console.log(enc(SECRET));
+  console.log(enc());
   console.log("\n----------------------------------------");
   console.log("Learning more about FS\n");
-  readFile(path.resolve(__dirname, "..", "greetings.txt"));
+  readFile();
+  readDir();
+  createDir("tmp");
+  copieFiles();
 });
